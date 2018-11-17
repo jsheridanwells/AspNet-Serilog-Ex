76e6f7f32b4ff4f8321a76dcdb7221332dd4a92a
Jeremy Wells <jeremywells@Jeremys-MacBook-Pro.local>
2018-11-17 12:16:41 -0600

    init

---
+++ LoggingEx.Api/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace LoggingEx.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            Log.Information("Pedimos el controlador de valores");
+            
+            try
+            {
+                throw new Exception("Ay! Hubo un error grave.");
+            }
+            catch(Exception ex)
+            {
+                Log.Error($"Error en ControladorDeValores :: {ex.ToString()}");
+            }
+            return Ok("la repuesta");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Asp.Net Serilog Example
+
+Boilerplate for settings up [Serilog](https://github.com/serilog/serilog-aspnetcore) console logging in an AspDotNet Core application.
